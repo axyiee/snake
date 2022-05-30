4a2af6f3c6ef7aa3a363824bf711fc32c5b1e78f
eexsty <hello@exst.fun>
2022-05-30 16:35:16 -0300

    feat: add support for gameplay states and create initial window

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use bevy::prelude::{App, Plugin};
+
 pub mod math;
 
 #[path = "player/plugin.rs"]
@@ -5,3 +7,40 @@ pub mod player;
 
 #[path = "macro/mod.rs"]
 mod macros;
+
+/// As recommended by usual game developers, we'll use states to separate
+/// logic in this game. Bevy has a built-in state system, so we'll use that:
+/// https://bevy-cheatbook.github.io/programming/states.html
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub enum GameplayState {
+    Loading,
+    Playing,
+    MainMenu,
+}
+
+impl GameplayState {
+    /// This function returns a description for each state.
+    pub fn description(&self) -> &'static str {
+        match &self {
+            Self::Loading => "Loading SFX assets...",
+            _ => "This state doesn't have a description.",
+        }
+    }
+}
+
+pub struct GamePlugin;
+
+impl Plugin for GamePlugin {
+    fn build(&self, app: &mut App) {
+        app.add_state(GameplayState::Loading)
+            .add_plugin(player::PlayerPlugin);
+
+        #[cfg(debug_assertions)]
+        {
+            use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
+
+            app.add_plugin(FrameTimeDiagnosticsPlugin::default())
+                .add_plugin(LogDiagnosticsPlugin::default());
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use bevy::prelude::{default, App, DefaultPlugins, Msaa, WindowDescriptor};
+use snake::GamePlugin;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .insert_resource(Msaa::default())
+        .insert_resource(WindowDescriptor {
+            title: String::from("Snake"),
+            ..default()
+        })
+        .add_plugins(DefaultPlugins)
+        .add_plugin(GamePlugin);
 }
