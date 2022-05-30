4d486f726bf2e96725e2461f620cbf18b7caf8c2
eexsty <hello@exst.fun>
2022-05-30 17:25:25 -0300

    feat: add z-axis support for the pythagoras theorem implementation

--- src/math/pythagoras.rs
+++ src/math/pythagoras.rs
@@ -3,5 +3,6 @@ use bevy::math::Vec3;
 pub fn pythagoras(from: Vec3, to: Vec3) -> f32 {
     let x = to.x - from.x;
     let y = to.y - from.y;
-    ((x * x) + (y * y)).sqrt()
+    let z = to.z - from.z;
+    ((x * x) + (y * y) + (z * z)).sqrt()
 }
