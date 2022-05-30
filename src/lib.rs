8ccecfb5655b5329f46e52ffd8c98356da52e8c5
eexsty <hello@exst.fun>
2022-05-30 15:54:38 -0300

    feat: add initial player plugin

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
 pub mod math;
 
+#[path = "player/plugin.rs"]
+pub mod player;
+
 #[path = "macro/mod.rs"]
 mod macros;
---
+++ src/player/plugin.rs
@@ -0,0 +1,12 @@
+use crate::import;
+use bevy::prelude::Plugin;
+
+pub mod entity;
+
+pub struct PlayerPlugin;
+
+impl Plugin for PlayerPlugin {
+    fn build(&self, app: &mut bevy::prelude::App) {
+        // todo
+    }
+}
