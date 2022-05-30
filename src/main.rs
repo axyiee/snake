61f2f1881bdd0b33000c4c3531dbc5f12b950f0b
eexsty <hello@exst.fun>
2022-05-30 20:25:19 -0300

    fix: change initial resolution for `wasm32` arch

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,26 @@
 use bevy::prelude::{default, App, DefaultPlugins, Msaa, WindowDescriptor};
 use snake::GamePlugin;
 
+#[allow(unused_mut)]
 fn main() {
     let mut app = App::new();
+    let mut resolution = (800., 600.);
+
+    #[cfg(target_arch = "wasm32")]
+    {
+        let document = web_sys::window().unwrap().document().unwrap().document_element().unwrap();
+        resolution = (document.client_width() as f32, document.client_height() as f32);
+        app.add_plugin(snake::web::WebViewportAutoResizePlugin);
+    }
+
     app.insert_resource(Msaa::default())
         .insert_resource(WindowDescriptor {
             title: String::from("Snake"),
-            height: 800.,
-            width: 600.,
+            width: resolution.0,
+            height: resolution.1,
             ..default()
         })
         .add_plugins(DefaultPlugins)
-        .add_plugin(GamePlugin);
-
-    #[cfg(target_arch = "wasm32")]
-    app.add_plugin(snake::web::WebViewportAutoResizePlugin);
-
-    app.run();
+        .add_plugin(GamePlugin)
+        .run();
 }
